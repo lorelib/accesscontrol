ec45e1d95df07d56f000306218805fe6082db3ee
listening <893979653@qq.com>
2017-03-30 17:40:22 +0800

    解决同一个线程短时间连续生成id相同的bug；进一步，添加随机数以保障id唯一性

--- commons/src/main/java/com/lorelib/hawk/infrastructure/helpers/algorithm/IdGenerator.java
+++ commons/src/main/java/com/lorelib/hawk/infrastructure/helpers/algorithm/IdGenerator.java
@@ -1,5 +1,7 @@
 package com.lorelib.hawk.infrastructure.helpers.algorithm;
 
+import org.apache.commons.lang3.RandomUtils;
+
 /**
  * id生成器
  * Created by listening on 2017/3/24.
@@ -9,6 +11,6 @@ public class IdGenerator {
     private final static long DEFAULT_DATACENTER_ID = 1L;
 
     public static long nextId() {
-        return new Snowflake(Thread.currentThread().getId(), DEFAULT_DATACENTER_ID, DEFAULT_SEQUENCE).nextId();
+        return new Snowflake(Thread.currentThread().getId() + RandomUtils.nextLong(), DEFAULT_DATACENTER_ID, DEFAULT_SEQUENCE).nextId();
     }
 }
