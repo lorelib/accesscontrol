350b86e5d5651d92643c3b4163a9e6ba6b1bf47f
listening <893979653@qq.com>
2017-04-20 11:48:17 +0800

    不合理的划分，查询条件对象不属于项目的构造块

--- commons/src/main/java/com/lorelib/hawk/infrastructure/stereotype/Criteria.java
+++
@@ -1,9 +0,0 @@
-package com.lorelib.hawk.infrastructure.stereotype;
-
-/**
- * @author listening
- * @description Criteria: 查询条件
- * @create 2017 04 14 13:39.
- */
-public interface Criteria extends DTO {
-}
